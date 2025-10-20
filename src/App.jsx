33b22f2bf7fdd2a1824b592fdebf7a699a3ebfaf
Adriano Souza <adrianoads910@gmail.com>
2025-10-20 12:53:40 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+const App = () => {
+return (
+<main className="min-h-dvh grid place-items-center bg-slate-50">
+<h1 className="text-3xl font-bold text-slate-800">
+Hello Tailwind + React!
+</h1>
+</main>
+);
+}
+export default App
